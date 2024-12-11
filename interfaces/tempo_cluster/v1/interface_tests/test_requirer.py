4b7a9e911539029739e385287257bd94c5b56d96
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-12-11 10:58:41 +0100

    json.dumps( on objects

--- interfaces/tempo_cluster/v1/interface_tests/test_requirer.py
+++ interfaces/tempo_cluster/v1/interface_tests/test_requirer.py
@@ -17,13 +17,13 @@ def test_data_on_created():
                     remote_app_name="coordinator",
                     remote_app_data={
                         "worker_config": json.dumps("foo: bar"),
-                        "charm_tracing_receivers": {
+                        "charm_tracing_receivers": json.dumps({
                             "otlp_http": "http://192.0.2.1:4318",
-                        },
-                        "workload_tracing_receivers": {
+                        }),
+                        "workload_tracing_receivers": json.dumps({
                             "otlp_http": "http://192.0.2.2:4318",
                             "otlp_grpc": "192.0.2.2:4317",
-                        },
+                        }),
                     },
                 )
             ]
